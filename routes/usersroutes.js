4b6bffa08b20deba043ec466e010e96241ea8d57
Nikos Giannaris <nikgiannsm@gmail.com>
2024-06-17 13:23:20 +0300

    Fixed password and password confirmation response error message

--- routes/usersroutes.js
+++ routes/usersroutes.js
@@ -3,21 +3,33 @@ const User = require('../Models/usersModel');
 const router = express.Router();
 
 router.post('/userSignIn', async (req , res) => {
+    const password = req.body.password;
+    const passwordConfirm = req.body.passwordConfirm;
+    
     try{
+     if (password === passwordConfirm){
        const data = new User ({
            name: req.body.name,
            email: req.body.email,
            password: req.body.password,
            passwordConfirm: req.body.passwordConfirm
-       })
-   
-       const newUser = await data.save(); //kanei save ta data pou stelnw
+       });
+
+       const newUser = await data.save(); 
        res.status(200).json(newUser);
        console.log('New user signed in succesfully.');
+    
+      }else{
+        res.status(401).json({
+            message:'Password and password confirmation are not the same.'
+        });
+        console.log('Password and password confirmation are not the same.');
+      };
+
     }catch{
-       res.status(400).json({
-           message:'Something went wrong while trying to sign in a new user.'
-       });
+        res.status(500).json({
+            message:'Something went wrong while trying to sign in a new user.'
+        });
     };
    });
 
