8fe58e17596d83028e73e5587d7d4a3194b6e955
Nikos Giannaris <nikgiannsm@gmail.com>
2024-06-25 17:31:01 +0300

    Fix code indentation

--- Models/bookModel.js
+++ Models/bookModel.js
@@ -1,40 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        require: true,
-        trim: true
-    },
+  title: {
+    type: String,
+    require: true,
+    trim: true,
+  },
 
-    author: {
-        type: String,
-        require: true,
-        trim: true
-    },
+  author: {
+    type: String,
+    require: true,
+    trim: true,
+  },
 
-    pages:{
-        type: Number,
-        required: true
-    },
+  pages: {
+    type: Number,
+    required: true,
+  },
 
-    genres:{
-        type: Array
-    },
+  genres: {
+    type: Array,
+  },
 
-    description:{
-        type: String,
-        required: true
-    },
+  description: {
+    type: String,
+    required: true,
+  },
 
-    rating:{
-        type: Number,
-        min: 0,
-        max:10
-    }
-
+  rating: {
+    type: Number,
+    min: 0,
+    max: 10,
+  },
 });
 
-const Book = mongoose.model('Book', bookSchema);
+const Book = mongoose.model("Book", bookSchema);
 
 module.exports = Book;
--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const mongoString = process.env.DATABASE_URL
-const app = require ('./app.js');
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const mongoString = process.env.DATABASE_URL;
+const app = require("./app.js");
 
-const port= 3000;
+const port = 3000;
 
 //MongoDB connection
 
 mongoose.connect(mongoString);
-const database = mongoose.connection
+const database = mongoose.connection;
 
-database.on('error', (error) => {
-    console.log(error)
-})
+database.on("error", (error) => {
+  console.log(error);
+});
 
-database.once('connected', () => {
-    console.log('Database Connected');
-})
+database.once("connected", () => {
+  console.log("Database Connected");
+});
 
 //Server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+  console.log(`Server is running on port ${port}`);
 });
-
